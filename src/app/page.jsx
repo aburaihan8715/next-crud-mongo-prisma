3116a0c28bef01ee55cc1ea4468e08aa352c4710
aburahan8721 <aburaihan8721@gmail.com>
2023-11-29 19:13:12 +0600

    hide .env

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,18 @@
+import DeleteTopicBtn from "@/components/topic/DeleteTopicBtn";
 import Link from "next/link";
+
+// async function getTopicsData() {
+//   const res = await fetch('https://api.example.com/...')
+//   // The return value is *not* serialized
+//   // You can return Date, Map, Set, etc.
+
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error('Failed to fetch data')
+//   }
+
+//   return res.json()
+// }
 
 export default function Home() {
   return (
@@ -10,31 +24,7 @@ export default function Home() {
             <p>Topic description</p>
           </div>
           <div className="flex gap-2 items-start">
-            <button className="c-btn bg-red-500 text-white">delete</button>
-            <Link href="/editTopic">
-              <button className="c-btn bg-green-500 text-white">edit</button>
-            </Link>
-          </div>
-        </li>
-        <li className="border py-6 px-4  flex justify-between">
-          <div>
-            <h2 className="text-2xl font-medium capitalize">Topic title</h2>
-            <p>Topic description</p>
-          </div>
-          <div className="flex gap-2 items-start">
-            <button className="c-btn bg-red-500 text-white">delete</button>
-            <Link href="/editTopic">
-              <button className="c-btn bg-green-500 text-white">edit</button>
-            </Link>
-          </div>
-        </li>
-        <li className="border py-6 px-4  flex justify-between">
-          <div>
-            <h2 className="text-2xl font-medium capitalize">Topic title</h2>
-            <p>Topic description</p>
-          </div>
-          <div className="flex gap-2 items-start">
-            <button className="c-btn bg-red-500 text-white">delete</button>
+            <DeleteTopicBtn />
             <Link href="/editTopic">
               <button className="c-btn bg-green-500 text-white">edit</button>
             </Link>
---
+++ src/components/topic/DeleteTopicBtn.jsx
@@ -0,0 +1,7 @@
+export default function DeleteTopicBtn() {
+  return (
+    <>
+      <button className="c-btn bg-red-500 text-white">delete</button>
+    </>
+  );
+}
