4f4d0b50fd0dee703c6c8344347bcbfbd7ae32e7
aburahan8721 <aburaihan8721@gmail.com>
2023-12-25 09:22:52 +0600

    update prisma client

--- src/libs/db.js
+++ src/libs/db.js
@@ -1,7 +1,25 @@
+// without typescript
 import { PrismaClient } from "@prisma/client";
 
-const client = globalThis.prisma || new PrismaClient();
+const prisma = globalThis.prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
+if (process.env.NODE_ENV !== "production") globalThis.prisma = prisma;
 
-export default client;
+export default prisma;
+
+// with typeScript
+// import { PrismaClient } from '@prisma/client'
+
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare global {
+//   var prisma: undefined | ReturnType<typeof prismaClientSingleton>
+// }
+
+// const prisma = globalThis.prisma ?? prismaClientSingleton()
+
+// export default prisma
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
